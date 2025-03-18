c36f50cca63775109bbee2debbbdf5642cc504c6
jacksonPontes14 <jacksonribeiro092@gmail.com>
2025-03-18 12:28:29 -0300

    UI is being created
    Organize the book cards

---
+++ main.js
@@ -0,0 +1,31 @@
+// Create a library app
+
+/* Part 1 */
+
+//books stored in array
+
+//Function that:
+//Can take some arguments
+//Create a book from this arguments
+//Then store the new book objct into an array
+
+//All books need have a unique idm use crypto.randomUUID
+
+
+/* Part 2 */
+
+// Function that loops through the array and display each book on the page
+
+//Read the 3.1 section
+
+/* Part 3 */
+
+// New book button for users
+
+/* Part 4 */
+
+// Button for removing books
+
+/* Part 5 */
+
+// Button that shows the book read status.
